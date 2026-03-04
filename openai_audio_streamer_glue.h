94d2f81387d0cd9945bac6f0fc5a55ddc65ef70f
Dario Pellegrino <dario.pellegrino@voismart.it>
2026-03-04 17:44:38 +0100

    fix: validate before alloc, guard empty resampled and minor fixes #27
    Signed-off-by: Dario Pellegrino <dario.pellegrino@voismart.it>

--- openai_audio_streamer_glue.h
+++ openai_audio_streamer_glue.h
@@ -4,7 +4,7 @@
 
 int validate_ws_uri(const char *url, char *wsUri);
 switch_status_t is_valid_utf8(const char *str);
-switch_status_t stream_session_send_json(switch_core_session_t *session, char *json);
+switch_status_t stream_session_send_json(switch_core_session_t *session, const char *json);
 switch_status_t stream_session_pauseresume(switch_core_session_t *session, int pause);
 switch_status_t stream_session_set_user_mute(switch_core_session_t *session, int mute);
 switch_status_t stream_session_set_openai_mute(switch_core_session_t *session, int mute);
